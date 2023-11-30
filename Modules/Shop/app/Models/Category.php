d3afbd929e42a026b1af48b9b1ef051619ded0cf
Zhillan Azmi <jilaann@backboxindonesia.or.id>
2023-12-01 00:31:06 +0700

    menambah function

--- Modules/Shop/app/Models/Category.php
+++ Modules/Shop/app/Models/Category.php
@@ -13,10 +13,31 @@ class Category extends Model
     /**
      * The attributes that are mass assignable.
      */
-    protected $fillable = [];
+
+    protected $table = 'shop_categories';
+    protected $fillable = [
+        'parent_id',
+        'slug',
+        'name',
+    ];
     
     protected static function newFactory(): CategoryFactory
     {
         //return CategoryFactory::new();
+    }
+
+    public function children()
+    {
+        return $this->hasMany('Modules\Shop\app\Models\Category', 'parent_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo('Modules\Shop\app\Models\Category', 'parent_id');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany('Modules\Shop\app\Models\Product', 'shop_categories_products', 'product_id', 'category_id');
     }
 }
