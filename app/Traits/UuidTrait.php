908239013d7915b4cd2dffcbd063300d1164d6e7
Zhillan Azmi <jilaann@backboxindonesia.or.id>
2023-11-30 20:53:14 +0700

    migrate db for login regist

---
+++ app/Traits/UuidTrait.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait UuidTrait
+{
+    /**
+     * Boot function from Laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            $model->incrementing = false;
+            $model->keyType = 'string';
+            $model->{$model->getKeyName()} = Str::uuid()->toString();
+        });
+    }
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
