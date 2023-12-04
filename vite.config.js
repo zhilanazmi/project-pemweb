2f1aa9d491d04494cf62e732df7bdd4aa1d95304
Sadira <rarasaf@gmail.com>
2023-12-04 17:19:38 +0700

    Memasang bootstrap

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/css/themes/pemweb/main.css',
             ],
             refresh: true,
         }),
